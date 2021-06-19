225977f4b20104db3af84c09824b316c3766ac3c
Nazan Bahar <77123580+nazanbahar@users.noreply.github.com>
2021-06-19 14:45:02 +0300

    Calling Signed Out.jsx and SingedIn.jsx components in Navi.jsx.

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,4 +1,60 @@
 import React from "react";
+import { Container, Menu } from "semantic-ui-react"; //for failed to compile error...
+import CartSummary from "./CartSummary";
+import SignedOut from "./SignedOut";
+import SignedIn from "./SignedIn";
+
+export default function Navi() {
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
+
+          <Menu.Menu position="right">
+            <CartSummary />
+
+            <SignedOut/>
+           <SignedIn/>
+          </Menu.Menu>
+        </Container>
+      </Menu>
+    </div>
+  );
+}
+
+/* ================================================================
+Navi.jsx NOTES SECTION -12 - STEPS
+step1:  cancelled manu.Item and button
+<Menu.Item>
+   <Button primary>Sign Up</Button>
+</Menu.Item>
+===============================================================================
+
+
+
+
+===============================================================================
+// SECTION -11 - STEPS
+//step1: rfc snippet code - rfc = reactFunctionalComponent
+//step2: return Semantic ui -menu bileşeni ekle
+//step3:  <Menu inverted> and  <Menu inverted fixed>
+//step4: <Container> POINT→ Menu içindekiler gelecek! </Container>
+//step5: Uyarı: Container eklerken de diğerlerinde oldğu gibi semantic-ui dan import etmek şarttır.
+//step6: Menu.Item → içerdeki elemanlar demektir.
+//step7: Menu.Menu → Menu içine alt menü eklemek istersek kullanabiliriz.
+//step8: CHANGED CODE: fixed="top" ekledik → <Menu inverted fixed="top">
+//step9: <Menu.Menu position="right"> içindeki Dropdown menuyu kestik.
+//step10: CartSummary.jsx'e paste et!
+// step11: Navi.jsx'de Dropdown 'ı taşıdığımız için, importunu da silmemiz gerekmektedir.
+//deleted  Dropdown import: import { Button, Container, Dropdown, Menu } from "semantic-ui-react";  //for failed to compile error...
+// code son hali: import { Button, Container, Menu } from "semantic-ui-react";  //for failed to compile error...
+//step12. Kullanmadığın import warning olarak çıkar. Kullanılmayan importu sil!
+//step13: <Menu inverted fixed="top"> ekleyelim.
+===================================================================
+Code Son Hali- SECTION -11
+import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react"; //for failed to compile error...
 import CartSummary from "./CartSummary";
 
@@ -21,28 +77,4 @@ export default function Navi() {
     </div>
   );
 }
-
-/* ================================================================
-Navi.jsx NOTES...
-===============================================================================
-
-===============================================================================
-// SECTION -11 - STEPS
-//step1: rfc snippet code - rfc = reactFunctionalComponent
-//step2: return Semantic ui -menu bileşeni ekle
-//step3:  <Menu inverted> and  <Menu inverted fixed>
-//step4: <Container> POINT→ Menu içindekiler gelecek! </Container>
-//step5: Uyarı: Container eklerken de diğerlerinde oldğu gibi semantic-ui dan import etmek şarttır.
-//step6: Menu.Item → içerdeki elemanlar demektir.
-//step7: Menu.Menu → Menu içine alt menü eklemek istersek kullanabiliriz.
-//step8: CHANGED CODE: fixed="top" ekledik → <Menu inverted fixed="top">
-//step9: <Menu.Menu position="right"> içindeki Dropdown menuyu kestik.
-//step10: CartSummary.jsx'e paste et!
-// step11: Navi.jsx'de Dropdown 'ı taşıdığımız için, importunu da silmemiz gerekmektedir.
-//deleted  Dropdown import: import { Button, Container, Dropdown, Menu } from "semantic-ui-react";  //for failed to compile error...
-// code son hali: import { Button, Container, Menu } from "semantic-ui-react";  //for failed to compile error...
-//step12. Kullanmadığın import warning olarak çıkar. Kullanılmayan importu sil!
-//step13: <Menu inverted fixed="top"> ekleyelim.
-===================================================================
-
 ===================================================================*/
